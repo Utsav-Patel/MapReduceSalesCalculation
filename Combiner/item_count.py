d1f3690ada30d4f798cb4c11b261c19f57642f26
Utsav Patel <Dell@macbook-pro.myfiosgateway.com>
2023-04-21 13:18:05 -0400

    Init commit

---
+++ Combiner/item_count.py
@@ -0,0 +1,52 @@
+import csv
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+from datetime import datetime
+
+K = 10
+DATA_PATH = "/Users/Dell/Documents/My Files/Rutgers/CS553 - Design of Internet Services/Project/Data/Sales_2019.csv"
+
+
+class MRItemCount(MRJob):
+
+    def __init__(self, *args, **kwargs):
+        super(MRItemCount, self).__init__(*args, **kwargs)
+        self.csv_reader = None
+
+    def mapper1_init(self):
+        self.csv_reader = csv.reader(open(DATA_PATH))
+
+    def mapper1(self, _, __):
+        for row in self.csv_reader:
+            if row[2].isnumeric():
+                yield row[1], int(row[2])
+
+    def combiner1(self, product, quantities):
+        yield product, sum(quantities)
+
+    def reducer1(self, product, quantities):
+        yield product, sum(quantities)
+
+    def mapper2(self, product, quantity):
+        yield None, (quantity, product)
+
+    def reducer2(self, _, tuples):
+        tuples = list(tuples)
+        tuples.sort(reverse=True)
+        for i in range(K):
+            yield i+1, ('Product: ' + tuples[i][1] + ' Quantity: ' + str(tuples[i][0]))
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper1, mapper_init=self.mapper1_init, combiner=self.combiner1, reducer=self.reducer1),
+            MRStep(mapper=self.mapper2, reducer=self.reducer2)
+        ]
+
+
+if __name__ == "__main__":
+
+    start_time = datetime.now()
+    MRItemCount.run()
+    end_time = datetime.now()
+
+    print('Time taken to complete MapReduce job: ', (end_time - start_time))
---
+++ main.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+data_df = pd.read_csv('Data/Sales_April_2019.csv')
+
